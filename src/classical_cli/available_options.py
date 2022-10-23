7910f59d1c14eb604c63f9918c5df5003a181f65
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-10-23 16:45:10 +0200

    basic cli options

--- src/classical_cli/available_options.py
+++ src/classical_cli/available_options.py
@@ -5,9 +5,26 @@ CLI_OPERATIONS = []
 
 def setup_available_options(argv):
     parser = argparse.ArgumentParser(add_help=False)
+    json_group = parser.add_argument_group('Read and write JSON file')
+    common_actions = parser.add_argument_group('Common actions')
 
     parser.add_argument('-h', '--help',
                         action='help',
                         help='Display this message.')
+    parser.add_argument('-f', '--file',
+                        metavar='',
+                        required=True,
+                        help='Path to file to operate on.')
+
+    json_group.add_argument('-r', '--read',
+                            action='store_true',
+                            help='Convert your JSON data into Prolog program.')
+    json_group.add_argument('-w', '--write',
+                            action='store_true',
+                            help='Convert your Prolog program into JSON data.')
+
+    common_actions.add_argument('-c', '--compile',
+                                action='store_true',
+                                help='Compile and execute Prolog program.')
 
     return parser.parse_args(argv)
