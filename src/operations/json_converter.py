58a5b800cd0ca9d9e57438ecc444e855c18ec73b
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-10-25 20:01:14 +0200

    minor fixes

--- src/operations/json_converter.py
+++ src/operations/json_converter.py
@@ -1,57 +1,7 @@
-from src.common.read_file import read_file
-from src.common.write_file import write_file
+from src.common.files.read_file import read_file
+from src.common.files.write_file import write_file
 
-from src.common.json_format_rules import JSON_FORMAT, ALLOWED_CONDITIONS_TYPES
-from src.common.exceptions import WrongJsonFormat, WrongFactFormat
-
-
-def check_json_format(data):
-    predicates = []
-    facts = []
-
-    for key, value in data.items():
-        if key not in JSON_FORMAT:
-            raise WrongJsonFormat
-        else:
-            for item_key, item_value in value.items():
-
-                if item_key not in JSON_FORMAT[key]:
-                    raise WrongJsonFormat
-                if type(item_value).__name__ != JSON_FORMAT[key][item_key]:
-                    raise WrongJsonFormat
-
-        if key == 'predicate':
-            predicates.append(value)
-
-        elif key == 'fact':
-
-            if len(value['conditions']) == 0:
-                raise WrongFactFormat
-
-            if 'arguments' not in value or type(value['arguments']).__name__ != 'list':
-                raise WrongFactFormat
-
-            if \
-                    "joins" not in value or \
-                    type(value['joins']).__name__ != 'list' or \
-                    len(value['joins']) != len(value['conditions']) - 1:
-                raise WrongFactFormat
-
-            for condition in value['conditions']:
-                if 'type' not in condition:
-                    raise WrongFactFormat
-                if condition['type'] not in ALLOWED_CONDITIONS_TYPES:
-                    raise WrongFactFormat
-                if condition['type'] == 'predicate':
-                    if 'arguments' not in condition or type(condition['arguments']).__name__ != 'list':
-                        raise WrongFactFormat
-
-            facts.append(value)
-
-    return {
-        'predicates': predicates,
-        'facts': facts
-    }
+from src.common.checkers.check_json_format import check_json_format
 
 
 def json_to_prolog(path_input_file, path_output_file, stdscr=None):
