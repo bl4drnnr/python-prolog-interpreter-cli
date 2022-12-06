4f8f1b900c5425e81072046036b7d4b78ad94386
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-12-06 21:09:06 +0100

    fixes

--- src/interactive_cli/execution.py
+++ src/interactive_cli/execution.py
@@ -7,9 +7,8 @@ from src.interactive_cli.docs import commands_docs
 
 from src.common.functions import print_raw_input, print_text
 
-from src.operations.json_converter import json_converter
-from src.operations.compile_prolog import compile_and_execute_prolog_program
-from src.operations.fetch_data import fetch_data
+from src.operations.json_toolbox import JsonParser, JsonFormatChecker
+from src.operations.prolog_toolbox import PrologParser, PrologFormatChecker
 
 
 def command_execution(stdscr, command):
@@ -29,21 +28,17 @@ def command_execution(stdscr, command):
             path_input_file = print_raw_input(stdscr, 'Please, provide path to input file: ').strip()
             path_output_file = print_raw_input(stdscr, 'Please, provide path for output file: ').strip()
 
-            json_converter('read', path_input_file, path_output_file, stdscr)
         elif command == 'Write to JSON':
             path_input_file = print_raw_input(stdscr, 'Please, provide path to input file: ').strip()
             path_output_file = print_raw_input(stdscr, 'Please, provide path for output file: ').strip()
 
-            json_converter('write', path_input_file, path_output_file, stdscr)
         elif command == 'Compile Prolog':
             path_input_file = print_raw_input(stdscr, 'Please, provide path to input file: ').strip()
             path_output_file = print_raw_input(stdscr, 'Please, provide path for output file: ').strip()
 
-            compile_and_execute_prolog_program(path_input_file, path_output_file, stdscr)
         elif command == 'Fetch data':
             url = print_raw_input(stdscr, 'Provide source of data you want to get: ').strip()
 
-            fetch_data(url)
     except WrongFactFormat:
         print_text('Wrong fact format!', stdscr, error=True)
     except WrongJsonFormat:
--- src/operations/json_converter.py
+++
@@ -1,7 +0,0 @@
-import json
-
-from src.common.files.read_file import read_file
-from src.common.files.write_file import write_file
-
-from src.common.functions import print_text
-
