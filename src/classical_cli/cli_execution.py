d83a67b0fa63e0fae527f92bc03b9a37919c0bb8
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-12-09 21:37:52 +0100

    cli execution refactoring

--- src/classical_cli/cli_execution.py
+++ src/classical_cli/cli_execution.py
@@ -7,6 +7,10 @@ from src.operations.json_toolbox.JsonFormatChecker import JsonFormatChecker
 from src.operations.prolog_toolbox.PrologParser import PrologParser
 from src.operations.prolog_toolbox.PrologFormatChecker import PrologFormatChecker
 from src.operations.compile.Compiler import Compiler
+from src.operations.fetch.Fetch import Fetch
+
+from src.common.files.read_file import read_file
+from src.common.files.write_file import write_file
 
 
 def check_for_options(options, required_options):
@@ -21,22 +25,40 @@ def cli_execution(operation, options):
             required_options = ['input_file', 'output_file']
             check_for_options(options, required_options)
 
-            json_converter('read', options['input_file'], options['output_file'])
+            json_format_checker = JsonFormatChecker()
+            json_parser = JsonParser()
+
+            file_data = read_file(options['input_file'])
+
+            json_data = json_format_checker.check_json_format(file_data)
+            output_program = json_parser.parse_json(json_data)
         elif operation == 'write':
             required_options = ['input_file', 'output_file']
             check_for_options(options, required_options)
 
-            json_converter('write', options['input_file'], options['output_file'])
+            prolog_format_checker = PrologFormatChecker()
+            prolog_parser = PrologParser()
+            
+            file_data = read_file(options['input_file'])
+
+            prolog_data = prolog_format_checker.check_prolog_format(file_data)
+            output_program = prolog_parser.parse_prolog(prolog_data)
         elif operation == 'compile':
             required_options = ['input_file', 'output_file']
             check_for_options(options, required_options)
 
-            compile_and_execute_prolog_program(options['input_file'], options['output_file'])
+            file_data = read_file(options['input_file'])
+            json_format_checker = JsonFormatChecker()
+            json_parser = JsonParser()
+
+            compiler = Compiler(json_parser, json_format_checker)
+            execution_result = compiler.execute_code(file_data)
         elif operation == 'fetch':
             required_options = ['url']
             check_for_options(options, required_options)
 
-            fetch_data(options['url'])
+            fetcher = Fetch()
+            # data = fetcher.fetch_data()
         else:
             raise WrongOption
     except WrongOption as wo:
